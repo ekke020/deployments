707a74b74c5351477b976a88207b1debda99cc2a
Johan Ekman <johan.ekman@pricer.com>
2022-12-05 11:25:42 +0100

    bumped test to 2 runid:3593891925 repo:Kuber envid:743920904

--- apply-config.sh
+++ apply-config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-curl -X POST -H Authorization: Bearer $API_TOKEN -H Content-Type: application/json \
-    https://api.github.com/repos/PricerAB/{repo}/actions/runs/{run_id}/pending_deployments \
-    -d \ 
-    {"environment_ids":['12313'],"state":"approved","comment":"Ship it!"}
+curl -v \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TEST" \
+  https://api.github.com/repos/ekk020/Kuber/actions/runs/3593891925/pending_deployments \
+  -d '{"environment_ids":[743920904],"state":"approved","comment":"Ship it!"}'
--- kuber-hook.py
+++ kuber-hook.py
@@ -16,8 +16,17 @@ spec:
         envFrom:
         - secretRef:
             name: kuber-secret
-        command: ["/bin/echo"]
-        args: ["$(API_TOKEN)"]
+        command:
+          - "curl"
+          - "-X"
+          - "POST"
+          - "-H"
+          - "Authorization: Bearer $API_TOKEN"
+          - "-H"
+          - "Content-Type: application/json"
+          - "https://api.github.com/repos/ekke020/{repo}/actions/runs/{run_id}/pending_deployments"
+          - "-d"
+          - "{{\\"environment_ids\\":[{env_id}],\\"state\\":\\"approved\\",\\"comment\\":\\"Ship it!\\"}}"
       restartPolicy: Never
   backoffLimit: 2"""
   return template
@@ -29,15 +38,3 @@ def generate():
 
 if __name__ == "__main__":
     generate()
-
-# command:
-#           - "curl"
-#           - "-X"
-#           - "POST"
-#           - "-H"
-#           - "Authorization: Bearer $API_TOKEN"
-#           - "-H"
-#           - "Content-Type: application/json"
-#           - "https://api.github.com/repos/ekke020/{repo}/actions/runs/{run_id}/pending_deployments"
-#           - "-d"
-#           - "{{\\"environment_ids\\":[{env_id}],\\"state\\":\\"approved\\",\\"comment\\":\\"Ship it!\\"}}"
